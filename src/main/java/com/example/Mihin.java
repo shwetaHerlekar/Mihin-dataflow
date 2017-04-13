07ed4da297d8bde72894900dc16b22e2bc4cf1e0
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-04-13 12:38:36 +0530

    Mihin dataflow

--- src/main/java/com/example/Mihin.java
+++ src/main/java/com/example/Mihin.java
@@ -99,7 +99,8 @@ public class Mihin{
 		Pipeline p = Pipeline.create(options);
 		CloudBigtableIO.initializeForWrite(p);
 		PCollection<String> lines=p.apply(TextIO.Read.named("Reading from File").from("gs://mihin-data/Patient_entry.txt"));
-		PCollection<String> line = lines.apply(Combine.globally(new AverageFn().withCoder(StringUtf8Coder.of())));
+		lines.setCoder(StringUtf8Coder);
+		PCollection<String> line = lines.apply(Combine.globally(new AverageFn()));
 		line.apply(TextIO.Write.to("gs://mihin-data/patients.txt"));
 		
 		//.apply(ParDo.named("Processing Synpuf data").of(MUTATION_TRANSFORM))
